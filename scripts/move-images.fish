4cda529d43ea59461c425203b54ae58104ddaa2f
redaphid <iam@aaronherres/com>
2023-03-14 16:57:02 -0700

    opa: single policy image. move scripts

--- scripts/move-images.fish
+++ scripts/move-images.fish
@@ -10,10 +10,12 @@ function move-images -d "Move images from Downloads to figures"
         set -l newname (echo $newname | sed -e 's/ /-/g')
         # to lowercase
         set -l newname (echo $newname | tr '[:upper:]' '[:lower:]')
-        echo $$oldname -> $newname
+        echo $oldname -> $newname
         mv $file ./figures/$newname
     end
 end
+
+move-images
  while true
     fswatch -1 5 ~/Downloads
     move-images

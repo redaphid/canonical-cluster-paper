a281408eb0b243812b8acc6f2800193e947286b5
redaphid <iam@aaronherres/com>
2023-03-14 13:08:35 -0700

    automate image moving. Instead of adding content

---
+++ move-images.fish
@@ -0,0 +1,20 @@
+#! /usr/bin/env fish
+
+function move-images -d "Move images from Downloads to figures"
+    for file in ~/Downloads/Canonical\ -\ *.jpg
+        set -l filename (basename $file)
+        # remove Canonical - from filename
+        set -l newname (echo $filename | sed -e 's/Canonical - //')
+
+        # replace spaces with dashes
+        set -l newname (echo $newname | sed -e 's/ /-/g')
+        # to lowercase
+        set -l newname (echo $newname | tr '[:upper:]' '[:lower:]')
+        echo $$oldname -> $newname
+        mv $file ./figures/$newname
+    end
+end
+ while true
+    fswatch -1 5 ~/Downloads
+    move-images
+end

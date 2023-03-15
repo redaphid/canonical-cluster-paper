ad9ef622185bf19ad5c0dab55ae100d0008069c9
redaphid <iam@aaronherres.com>
2023-03-14 20:20:42 -0700

    Make fswatch less chatty in linux. Add conclusion

--- scripts/generate-json.fish
+++ scripts/generate-json.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 #  yq -o json -p yaml ./messages/message.yaml > ./message.json
  while true
-  fswatch -1 5 ./messages
+  fswatch -1 5 --one-event ./messages/message.yaml
   yq -o json -p yaml ./messages/message.yaml > ./message.json
 end
 
